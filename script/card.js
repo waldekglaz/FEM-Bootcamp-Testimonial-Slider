14ee3dbeeafaa2ced0e8001afbd75415f2e01dac
waldekglaz <waldekglaz@gmail.com>
2021-12-27 10:34:21 +0100

    slider logic

--- script/card.js
+++ script/card.js
@@ -1,25 +1,42 @@
-const img = document.querySelector('.main-img');
-const source = img.src;
-const text = document.querySelector('.card-text');
-const person = document.querySelector('.author-name');
-const role = document.querySelector('.author-role');
+const content = [
+    {
+        id:"1",
+        text:`“ I’ve been interested in coding for a while but never taken the jump, until now.
+        I couldn’t recommend this course enough. I’m now in the job of my dreams and so
+        excited about the future. ”`,
+        name:"Tanya Sinclair",
+        role:"UX Engineer"
+    },
+    {
+        id:"2",
+        text:`“ If you want to lay the best foundation possible I’d recommend taking this course.
+        The depth the instructors go into is incredible. I now feel so confident about
+        starting up as a professional developer. ”`,
+        name:"John Tarkpor",
+        role:"Junior Front-end Developer"
+    }
+];
+const img = document.querySelector('.image-slider__image');
+let imgSource = img.src;
+const text = document.querySelector('.testimonial__quote');
+const person = document.querySelector('.testimonial__name');
+const role = document.querySelector('.testimonial__role');
 
-const nextBtn = document.querySelector('.nextBtn');
-const prevBtn = document.querySelector('.prevBtn');
-
-nextBtn.addEventListener('click', () => {
-    img.src = 'images/image-john.jpg';
-    text.textContent = `“ If you want to lay the best foundation possible I’d recommend taking this course.
-    The depth the instructors go into is incredible. I now feel so confident about
-    starting up as a professional developer. ”`;
-    person.textContent = 'John Tarkpor';
-    role.textContent = 'Junior Front-end Developer'
-})
-prevBtn.addEventListener('click', () => {
-    img.src = 'images/image-tanya.jpg';
-    text.textContent = `“ I’ve been interested in coding for a while but never taken the jump,
-    until now. I couldn’t recommend this course enough. I’m now in the job
-    of my dreams and so excited about the future. ”`;
-    person.textContent = 'Tanya Sinclair';
-    role.textContent = 'UX Engineer'
-})
+const btns = document.querySelectorAll('.btn')
+const changeSlide = () =>{
+    if(person.textContent === content[0].name){
+        img.src = "images/image-john.jpg"
+        person.textContent = content[1].name;
+        role.textContent = content[1].role;
+        text.textContent = content[1].text;
+    }
+    else{
+        img.src = "images/image-tanya.jpg"
+        person.textContent = content[0].name;
+        role.textContent = content[0].role;
+        text.textContent = content[0].text;
+    }
+}
+btns.forEach(btn => {
+    btn.addEventListener('click', changeSlide)
+});
